a7ac05f53c5369bf1e850ade3440d59f29a50c7d
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-18 12:54:04 +0530

    Update codePush.groovy

--- vars/codePush.groovy
+++ vars/codePush.groovy
@@ -36,7 +36,7 @@ def sout = new StringBuffer(), serr = new StringBuffer()
 
 def proc ='../resources/codePush.sh'.execute()
 
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(1000)
+//proc.consumeProcessOutput(sout, serr)
+//proc.waitForOrKill(1000)
 println sout
 }
