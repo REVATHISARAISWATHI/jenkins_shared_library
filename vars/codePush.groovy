968adf1ccaac1ac3e5fe75173fc45f674f845732
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-21 15:49:07 +0530

    Update codePush.groovy

--- vars/codePush.groovy
+++ vars/codePush.groovy
@@ -8,49 +8,24 @@ import hudson.plugin.git.extension.impl.*
   codePush(String data){
     def jsonSlurper = new JsonSlurper() 
     def resultJson = jsonSlurper.parseText(data)
-   // def projectName = '"'+resultJson.name+'"'
+   
     def gitUrl= resultJson.url
     
     echo gitUrl
   
-  //def link1 = "rm -rf ./* && git clone https://github.com/amanchourasia/JenkinsWar.git"
-  def link1 = "rm -rf ./* && git clone ${gitUrl} && git clone http://rig:${rig_password}@${BB_URL}/scm/DEM/app.git && cp -r ./JenkinsWar/* ./app && pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git push -u origin master"
-  //def link2 = "git clone http://rig:${rig_password}@${BB_URL}/scm/DEM/app.git"
-  //def link3 = "cp -r ./JenkinsWar/* ./app"
-  //def link4 = "pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git push -u origin master"
   
+  def link1 = "rm -rf ./* && git clone ${gitUrl} && git clone http://rig:${rig_password}@${BB_URL}/scm/DEM/app.git && cp -r ./JenkinsWar/* ./app && pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git push -u origin master"
+   
    echo "************** Aman Repo ************"
    
    sh(link1)
-   //sh(link2)
-   //sh(link3)
-   //sh(link4)
+   
     
   }
 
          
  def call(){
- def request = libraryResource 'data.json'
-   /*def jsonSlurper = new JsonSlurper() 
-    def resultJson = jsonSlurper.parseText(request)
-   // def projectName = '"'+resultJson.name+'"'
-    def gitUrl= resultJson.url
-    
-    echo gitUrl
-  
-  def link1 = "rm -rf ./* && git clone https://github.com/amanchourasia/JenkinsWar.git"
-  //def link1 = "rm -rf ./* && git clone ${gitUrl}"
-  def link2 = "git clone http://rig:${rig_password}@${BB_URL}/scm/DEM/app.git"
-  def link3 = "cp -r ./JenkinsWar/* ./app"
-  def link4 = "pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git push -u origin master"
-  
-   echo "************** Aman Repo ************"
-   
-   sh(link1)
-   sh(link2)
-   sh(link3)
-   sh(link4)*/
-   
+ def request = libraryResource 'data.json'   
  codePush(request)
  }               
    
