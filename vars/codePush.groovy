21cf74311456d2924b5d0d69a103e5e858b3e648
mishraanupama661 <32787676+mishraanupama661@users.noreply.github.com>
2019-10-21 13:05:46 +0530

    Update codePush.groovy

--- vars/codePush.groovy
+++ vars/codePush.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurper
 jenkins = Jenkins.instance
 import hudson.plugin.git.extension.impl.*
   
-  @NonCPS
+ /* @NonCPS
   codePush(String data){
     def jsonSlurper = new JsonSlurper() 
     def resultJson = jsonSlurper.parseText(data)
@@ -21,16 +21,35 @@ import hudson.plugin.git.extension.impl.*
   
    echo "************** Aman Repo ************"
    
-   sh(link1,link2)
-   //sh(link2)
-   //sh(link3)
-  // sh(link4)
+   sh(link1)
+   sh(link2)
+   sh(link3)
+   sh(link4)
     
-  }
+  }*/
 
          
  def call(){
  def request = libraryResource 'data.json'
- codePush(request)
+   def jsonSlurper = new JsonSlurper() 
+    def resultJson = jsonSlurper.parseText(request)
+   // def projectName = '"'+resultJson.name+'"'
+    def gitUrl= resultJson.url
+    
+    echo gitUrl
+  
+  def link1 = "rm -rf ./* && git clone https://github.com/amanchourasia/JenkinsWar.git"
+  //def link1 = "rm -rf ./* && git clone ${gitUrl}"
+  def link2 = "git clone http://rig:${rig_password}@${BB_URL}/scm/DEM/app.git"
+  def link3 = "cp -r ./JenkinsWar/* ./app"
+  def link4 = "pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git push -u origin master"
+  
+   echo "************** Aman Repo ************"
+   
+   sh(link1)
+   sh(link2)
+   sh(link3)
+   sh(link4)
+ //codePush(request)
  }               
    
